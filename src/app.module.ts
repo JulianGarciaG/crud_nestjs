af0f5c3f66e686e87433843102125e931309e3f3
Julian <juliangarciag8522@gmail.com>
2025-06-20 15:45:16 -0500

    se agrego diferentes modulos, para agregar las variables de entrono de mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TaskModule } from './task/task.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (Config: ConfigService) => ({
+        type: 'mysql', 
+        host: Config.get('DB_HOST'),
+        port: Config.get('DB_PORT'),
+        username: Config.get('DB_USER'),
+        database: Config.get('DB_NAME'),
+        entities: [__dirname + '/**/*/*.entity{.ts,.js}'],
+        synchronize: true,
+
+      }),
+    }),
+    TaskModule,
+  ],
 })
 export class AppModule {}
