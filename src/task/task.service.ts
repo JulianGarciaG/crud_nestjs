7e1ae181730683dc3eb2978ab8b9101f8c720678
Julian <juliangarciag8522@gmail.com>
2025-06-20 17:13:47 -0500

    implementacion de taskservice en el CRUD

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    @InjectRepository(Task)
+    private readonly taskRepository: Repository<Task>;
+
+    async findAll(): Promise<Task[]>{
+        return this.taskRepository.find();
+    }
+
+    async findOne(id: number): Promise<Task>{
+        const task = await this.taskRepository.findOneBy({id});
+        if(!task){
+            throw new HttpException('Task not found', HttpStatus.NOT_FOUND,);
+        }
+        return task;
+    }
+
+    async create(task : Task): Promise<Task>{
+        const newTask = await this.taskRepository.create(task)
+        return this.taskRepository.save(newTask)
+    }
+
+    async update(id: number, task: Task): Promise<Task>{
+        const existingTask = await this.findOne(id);
+        if(!existingTask){
+            throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+        }
+        const updatedTask = Object.assign(existingTask, task);
+        return this.taskRepository.save(updatedTask);
+        
+    }
+
+    async delete(id: number): Promise<void>{
+        const result = await this.taskRepository.delete(id);
+        if(result.affected === 0){
+            throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+        };
+
+        
+    }
+
+}
